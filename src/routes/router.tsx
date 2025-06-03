aa792ac11777677680048bd0401043864b840663
Leonildo-Gomes <leogomescv@gmail.com>
2025-06-03 16:40:45 +0200

    Feat: first commit, addd project strukture

---
+++ src/routes/router.tsx
@@ -0,0 +1,35 @@
+
+import { createBrowserRouter } from 'react-router-dom';
+import { Layout } from '../components/layout';
+import { Dashboard } from '../pages/dashboard';
+import { Home } from '../pages/home';
+import { Login } from '../pages/login';
+import { Register } from '../pages/register';
+
+const router = createBrowserRouter([
+    {  
+        element: <Layout/>, 
+        children: [
+            {
+                path: '/',
+                element: <Home/>
+            },
+            {
+                path: '/register',
+                element: <Register/>
+            },
+            {
+                path: '/dashboard',
+                element: <Dashboard/>
+            },
+            {
+                path: '/login',
+                element: <Login/>
+            }
+        ] 
+
+    }
+   
+])
+
+export { router };
